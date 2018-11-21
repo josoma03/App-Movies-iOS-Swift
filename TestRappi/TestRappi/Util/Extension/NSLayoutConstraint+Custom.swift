6b805b7675398f33be69eef537a9c86537723d02
Jhonattan Solarte Martinez <jhonattan.solarte@arandasoft.com>
2018-11-21 09:48:23 -0500

    Mejora en la interfaz del listado y del detalle Incluyo pod XCDYouTubeKit para visualizar videos de youtube en el detalle de la película

---
+++ TestRappi/TestRappi/Util/Extension/NSLayoutConstraint+Custom.swift
@@ -0,0 +1,9 @@
+//
+//  NSLayoutConstraint+Custom.swift
+//  TestRappi
+//
+//  Created by Jhonattan Solarte Martinez on 21/11/18.
+//  Copyright © 2018 Rappi. All rights reserved.
+//
+
+import Foundation
